495dd22e62cf03c2b8f34b3d1a452597ea514c22
Alessandra <asaracini@ethz.ch>
2023-08-01 18:47:58 +0200

    descriptions. done

--- R/simulate_meta_data.R
+++ R/simulate_meta_data.R
@@ -2,66 +2,66 @@
 #' @export
 #'
 
-#To simulate one data set of a meta analysis with and without ORB
+#To simulate one data set of a meta analysis with  ORB
 simulate_meta_data <- function(n_studies, mu, tau_squared, n_treatment=50, n_control=50, gamma){
 
-  if (!require("biostatUZH")) {
+  if (!require("dplyr")) {
     # If the package is not loaded, try to load it
-    if (!requireNamespace("biostatUZH", quietly = TRUE)) {
+    if (!requireNamespace("dplyr", quietly = TRUE)) {
       # If the package is not installed, give an error message
-      stop("The 'biostatUZH' package is not installed.")
+      stop("The 'dplyr' package is not installed.")
     } else {
       # If the package is installed but not loaded, load it
-      library("biostatUZH")
+      library("dplyr")
     }
   }
 
 
 
-#Number of studies per meta-analysis
-n_studies <- n_studies
+  #Number of studies per meta-analysis
+  n_studies <- n_studies
 
-#True treatment effect and true heterogeneity parameter
-mu <- mu
-tau_squared <- tau_squared
+  #True treatment effect and true heterogeneity parameter
+  mu <- mu
+  tau_squared <- tau_squared
 
-#Empty data frame
-meta_data <- data.frame(study = 1:n_studies,
-                        n_control = numeric(n_studies),
-                        n_treatment = numeric(n_studies),
-                        y = numeric(n_studies),
-                        s = numeric(n_studies),
-                        p_value = numeric(n_studies))
+  #Empty data frame
+  meta_data <- data.frame(study = 1:n_studies,
+                          n_control = numeric(n_studies),
+                          n_treatment = numeric(n_studies),
+                          y = numeric(n_studies),
+                          s = numeric(n_studies),
+                          p_value = numeric(n_studies))
 
 
-for (i in 1:n_studies){
+  for (i in 1:n_studies){
 
-  #Sample sizes
-  n_treatment <- n_treatment
-  n_control <- n_control
+    #Sample sizes
+    n_treatment <- n_treatment
+    n_control <- n_control
 
-  #Generate true treatment effect mean and standard errors
-  theta_i <- rnorm(1, mean = mu, sd = sqrt(tau_squared))
+    #Generate true treatment effect mean and standard errors
+    theta_i <- rnorm(1, mean = mu, sd = sqrt(tau_squared))
 
-  #Standard errors from Chisquare distribution
+    #Standard errors from Chisquare distribution
 
-  sigma_i <- sqrt(rchisq(1, df= 2*n_control - 2)/((n_control -1)*n_control))
+    sigma_i <- sqrt(rchisq(1, df= 2*n_control - 2)/((n_control -1)*n_control))
 
-  #Generate observed treatment effect
-  y_i <- rnorm(1, mean = theta_i, sd = sigma_i)
+    #Generate observed treatment effect
+    y_i <- rnorm(1, mean = theta_i, sd = sigma_i)
 
-  #Calculate one-sided p-value
-  p_i <- pnorm(y_i / sigma_i, lower.tail = FALSE)
-  p_i <- round(p_i, 10)
+    #Calculate one-sided p-value
+    p_i <- pnorm(y_i / sigma_i, lower.tail = FALSE)
+    p_i <- round(p_i, 10)
 
-  #Fill in data frame
-  meta_data[i, "n_control"] <- n_control
-  meta_data[i, "n_treatment"] <- n_treatment
-  meta_data[i, "y"] <- y_i
-  meta_data[i, "s"] <- sigma_i
-  meta_data[i, "p_value"] <- p_i
+    #Fill in data frame
+    meta_data[i, "n_control"] <- n_control
+    meta_data[i, "n_treatment"] <- n_treatment
+    meta_data[i, "y"] <- y_i
+    meta_data[i, "s"] <- sigma_i
+    meta_data[i, "p_value"] <- p_i
 
-}
+  }
 
   #Missing data mechanism
   meta_data_miss <- meta_data
@@ -78,47 +78,45 @@ for (i in 1:n_studies){
     } else {
       meta_data_miss$y[j] <- meta_data_miss$y[j]
       meta_data_miss$s[j] <- meta_data_miss$s[j]
-      }
+    }
 
-}
+  }
 
 
   #among the rest, choose some at random some to be low risk
   if (length(which(meta_data_miss$y == "high")) > 0){ #if there is at least one HR
 
-  non_high_indices <- which(meta_data_miss$y != "high") #indecies reported
-  n_high_rows <- length(which(meta_data_miss$y == "high")) #number of HR
-  n_values_to_replace <- min(sample(0:n_high_rows, 1), length(non_high_indices)) #so that never more low than high
-  replace_indices <- sample(non_high_indices, n_values_to_replace)
-  meta_data_miss$y[replace_indices] <- "low"
-  meta_data_miss$s[replace_indices] <- "low"
+    non_high_indices <- which(meta_data_miss$y != "high") #indecies reported
+    n_high_rows <- length(which(meta_data_miss$y == "high")) #number of HR
+    n_values_to_replace <- min(sample(0:n_high_rows, 1), length(non_high_indices)) #so that never more low than high
+    replace_indices <- sample(non_high_indices, n_values_to_replace)
+    meta_data_miss$y[replace_indices] <- "low"
+    meta_data_miss$s[replace_indices] <- "low"
 
-} else {
+  } else {
 
-  meta_data_miss$y <- meta_data_miss$y
-  meta_data_miss$s <- meta_data_miss$s
-}
-
-
-
-#make sure we have at least 3 studies that are reported otheriwse calculationg of ci unstable
-#pls also v unrealistic that there would be a meta analysis conducted on 2 studies!
+    meta_data_miss$y <- meta_data_miss$y
+    meta_data_miss$s <- meta_data_miss$s
+  }
 
 
+
+  #make sure we have at least 3 studies that are reported otheriwse calculationg of ci unstable
+  #pls also v unrealistic that there would be a meta analysis conducted on 2 studies!
   non_high_low_rows <- which(!(meta_data_miss$y %in% c("high", "low")))
   n_non_high_low_rows <- length(non_high_low_rows)
 
   if (n_non_high_low_rows < 3) {
-  high_low_rows <- which(meta_data_miss$y %in% c("high", "low"))
+    high_low_rows <- which(meta_data_miss$y %in% c("high", "low"))
 
-  if (n_non_high_low_rows == 0) {
-    random_rows <- sample(high_low_rows, 3)
-  } else {
-    random_rows <- sample(high_low_rows, 3 - n_non_high_low_rows)
-  }
+    if (n_non_high_low_rows == 0) {
+      random_rows <- sample(high_low_rows, 3)
+    } else {
+      random_rows <- sample(high_low_rows, 3 - n_non_high_low_rows)
+    }
 
-  meta_data_miss$y[random_rows] <- meta_data$y[random_rows]
-  meta_data_miss$s[random_rows] <- meta_data$s[random_rows]
+    meta_data_miss$y[random_rows] <- meta_data$y[random_rows]
+    meta_data_miss$s[random_rows] <- meta_data$s[random_rows]
   }
 
   meta_data_miss$s_true <- meta_data$s
@@ -126,9 +124,5 @@ for (i in 1:n_studies){
   return(meta_data_miss)
 
 }
-
-
-
-
 
 
